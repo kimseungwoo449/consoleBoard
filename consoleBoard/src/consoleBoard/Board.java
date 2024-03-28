167a71a66ff608f03fc3d8b474fc1a145e2a2c54
kimseungwoo449 <ehdanf449@naver.com>
2024-03-28 18:19:45 +0900

    add : 생성자, getInstance method

--- consoleBoard/src/consoleBoard/Board.java
+++ consoleBoard/src/consoleBoard/Board.java
@@ -1,5 +1,22 @@
 package consoleBoard;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+	private Map<Map<String, String>, Map<String, String>> board;
 	
+	private static Board instance = new Board();
+	
+	private Board() {
+		board = new HashMap<Map<String,String>, Map<String,String>>();
+		Map<String,String> idAndPassword = new HashMap<String, String>();
+		Map<String,String> titleAndDetail = new HashMap<String, String>();
+	
+		board.put(idAndPassword, titleAndDetail);
+	}
+	
+	public Board getInstance() {
+		return instance;
+	}
 }
