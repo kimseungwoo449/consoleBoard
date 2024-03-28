57541b9a73578ca232bdc4166e00ea9ff1e8b26f
kimseungwoo449 <ehdanf449@naver.com>
2024-03-28 17:35:27 +0900

    modify : 추상 개념 지움

--- consoleBoard/src/consoleBoard/User.java
+++ consoleBoard/src/consoleBoard/User.java
@@ -3,10 +3,10 @@ package consoleBoard;
 import java.util.HashMap;
 import java.util.Map;
 
-abstract public class User {
-	public String id;
-	public String password;
-	public Map<String, String> myBoard;		// 나의 게시판 목록
+public class User {
+	private String id;
+	private String password;
+	private Map<String, String> myBoard;		// 나의 게시판 목록
 	
 	public User(String id,String password) {
 		this.id = id;
