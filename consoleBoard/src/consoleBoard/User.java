15cb7d00ab41313d6cb9c6b145eddfbe5f214a65
kimseungwoo449 <ehdanf449@naver.com>
2024-03-28 17:31:19 +0900

    add : 필드값, 생성자

--- consoleBoard/src/consoleBoard/User.java
+++ consoleBoard/src/consoleBoard/User.java
@@ -1,5 +1,18 @@
 package consoleBoard;
 
-public class User {
+import java.util.HashMap;
+import java.util.Map;
 
+abstract public class User {
+	public String id;
+	public String password;
+	public Map<String, String> myBoard;		// 나의 게시판 목록
+	
+	public User(String id,String password) {
+		this.id = id;
+		this.password = password;
+		this.myBoard = new HashMap<String, String>();
+	}
+	
+	
 }
