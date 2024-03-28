c93516caf754557f2395eff05567ee869d05f287
kimseungwoo449 <ehdanf449@naver.com>
2024-03-28 17:46:45 +0900

    delete : myBoard

--- consoleBoard/src/consoleBoard/User.java
+++ consoleBoard/src/consoleBoard/User.java
@@ -1,18 +1,20 @@
 package consoleBoard;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class User {
 	private String id;
 	private String password;
-	private Map<String, String> myBoard;		// 나의 게시판 목록
 	
 	public User(String id,String password) {
 		this.id = id;
 		this.password = password;
-		this.myBoard = new HashMap<String, String>();
 	}
 	
+	public String getId() {
+		return this.id;
+	}
 	
+	public String getPassword() {
+		return this.password;
+	}
 }
