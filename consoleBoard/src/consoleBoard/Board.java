42857c2f8b390bdfc2879f405189762eca8b935e
kimseungwoo449 <ehdanf449@naver.com>
2024-03-29 10:01:02 +0900

    add : createStringInfo method add : viewContents method

--- consoleBoard/src/consoleBoard/Board.java
+++ consoleBoard/src/consoleBoard/Board.java
@@ -1,16 +1,28 @@
 package consoleBoard;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
 	private final String EXIT = "*";
-	private Map<String[], String> board;
 	
+	private final int CONTENTS_NUMBER = 0;
+	private final int ID = 1;
+	private final int PASSWORD = 2;
+	private final int TITLE = 3;
+	
+	
+	private Map<String[], String> board;
+	private int contentsNumber;
 	private static Board instance = new Board();
 
 	private Board() {
 		board = new HashMap<String[], String>();
+		this.contentsNumber = 1;
 	}
 
 	public Board getInstance() {
@@ -18,7 +30,7 @@ public class Board {
 	}
 
 	// 게시글 작성
-	public void writing(String number,String id,String password) {
+	public void writing(String id,String password) {
 		String title = ConsoleBoard.inputString("제목");
 		String detail = "";
 		System.out.println("내용 (*입력시 종료): ");
@@ -31,25 +43,44 @@ public class Board {
 		}
 		
 		String[] keyValues = new String[4];
-		keyValues[0] = number;
-		keyValues[1]= id;
-		keyValues[2] = password;
-		keyValues[3] = title;
+		keyValues[CONTENTS_NUMBER] = contentsNumber+"";
+		keyValues[ID]= id;
+		keyValues[PASSWORD] = password;
+		keyValues[TITLE] = title;
 		
-		
+		contentsNumber++;
 		board.put(keyValues, detail);
 		System.out.println("게시글 등록 완료.");
 	}
 	
 	// 읽기
-	public void viewContents(String title) {
+	public void viewContents() {
+		String number = ConsoleBoard.inputString("조회하려는 글 번호");
 		
+		String contents = createStringInfo(number);
+		if(contents==null) {
+			System.err.println("찾으시는 글번호는 존재하지 않습니다.");
+			return;
+		}
 	}
 	
-	private String[] findContentsByTitle(String title) {
-		String[] info = null;
+	private String createStringInfo(String number) {
+		String info = null;
 		
-		
+		for(String[] key:board.keySet()) {
+			String keyNumber = key[CONTENTS_NUMBER];
+			if(number.equals(keyNumber)) {
+				String title = key[TITLE];
+				String detail = board.get(key);
+				info = String.format("제목 : %s\n내용 : %s",title,detail );
+				return info;
+			}
+		}
 		return info;
 	}
+	
+//	public void viewAllTitle() {
+//		List keySet = new ArrayList(board.keySet());
+//		Collections.sort(keySet, );;
+//	}
 }
