e846cc9d5ef65fa9c9870df779ef102709ffa2d6
kimseungwoo449 <ehdanf449@naver.com>
2024-03-29 10:14:15 +0900

    add : getters

--- consoleBoard/src/consoleBoard/Board.java
+++ consoleBoard/src/consoleBoard/Board.java
@@ -1,22 +1,31 @@
 package consoleBoard;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Board {
 	private String id;
 	private String password;
 	private String title;
 	private String detail;
-	
-	public Board(String id,String password,String title,String detail) {
+
+	public Board(String id, String password, String title, String detail) {
 		this.id = id;
 		this.password = password;
 		this.title = title;
 		this.detail = detail;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getDetail() {
+		return this.title;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public String getPassword() {
+		return this.password;
 	}
 }
